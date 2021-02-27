cce97b3a78be293b7b7034706cf09d0481a1b99f
Kieran-Reilly <kieranpreilly@gmail.com>
2021-02-27 15:57:21 +0200

    Initial attempt

--- index.js
+++ index.js
@@ -1,27 +1,90 @@
-import {initialize} from "./src/initialize.js";
-import {addCube} from "./src/shapes.js";
+import {Particle} from "./src/particle.js";
 
-const cameraLocation = {z: 5}
-const rendererParameters = {antialias: true} // see WebGLRendererParameters
-const lightProperties = {
-    x: 5,
-    y: 5,
-    z: 7.5,
-    color: 0xffffff,
-    integrity: 1.0
+//initialize the canvas
+const canvas = document.createElement("canvas");
+const context = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const particles = [];
+let hue = 0;
+
+window.addEventListener('resize', function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+})
+
+const mouseHandler = {
+    x: null,
+    y: null,
 }
 
-const sceneObj = initialize(cameraLocation, rendererParameters, lightProperties);
-const cube = addCube(sceneObj, 0xff0080);
+canvas.addEventListener('click', async function (event) {
+    mouseHandler.x = event.x;
+    mouseHandler.y = event.y;
+    for (let i = 0; i < 10; i++) {
+        particles.push(new Particle(mouseHandler.x, mouseHandler.y, context, hue));
+    }
+});
 
-sceneObj.light.target = cube;
+canvas.addEventListener("mousemove", async function (event) {
+    mouseHandler.x = event.x;
+    mouseHandler.y = event.y;
+    for (let i = 0; i < 10; i++) {
+        particles.push(new Particle(mouseHandler.x, mouseHandler.y, context, hue));
+    }
+} )
 
-const animate = () => {
+// function drawCircle(x, y){
+//     context.fillStyle = 'blue';
+//     context.beginPath();
+//     context.arc(x, y, 50, 0, Math.PI * 2);
+//     context.fill();
+// }
+
+// function initParticles() {
+//     for (let i = 0; i < 100; i++) {
+//         particles.push(new Particle(mouseHandler.x, mouseHandler.y, context));
+//     }
+// }
+// initParticles();
+
+function particlesHandler() {
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].update();
+        particles[i].draw();
+        //connecting particles if they are within a certain range of each other
+        for (let j = i; j < particles.length; j++) {
+            const dx = particles[i].x - particles[j].x;
+            const dy = particles[i].y - particles[j].y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+            if (distance < 20) {
+                context.beginPath();
+                context.strokeStyle = particles[i].colour;
+                context.lineWidth = 0.2;
+                context.moveTo(particles[i].x, particles[i].y);
+                context.lineTo(particles[j].x, particles[j].y);
+                context.stroke();
+                context.closePath();
+            }
+        }
+        //if particle shrinks below a certain size, remove it from the array
+        if (particles[i].size <= 0.3) {
+            particles.splice(i, 1);
+            i--;
+        }
+
+    }
+}
+
+function animate() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    // context.fillStyle = 'rgba(0,0,0,0.02)';
+    // context.fillRect(0, 0, canvas.width, canvas.height);
+    particlesHandler();
+    hue++;
     requestAnimationFrame(animate);
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    sceneObj.renderer.render(sceneObj.scene, sceneObj.camera);
 }
 
 animate();
+
+document.querySelector("body").appendChild(canvas);
---
+++ src/particle.js
@@ -0,0 +1,39 @@
+export class Particle {
+    constructor(mouseX, mouseY, context, hue) {
+        this.context = context
+        this.x = mouseX;
+        this.y = mouseY;
+        this.size = Math.random() * 5 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+        this.colour = 'hsl(' + hue + ', 100%, 50%)'
+    }
+
+    dispose() {
+        this.x = null;
+        this.y = null;
+        this.size = null;
+        this.speedX = null;
+        this.speedY = null
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.size > 0.2) this.size -= 0.1;
+    }
+
+    draw() {
+        this.context.fillStyle = this.colour;
+        this.context.beginPath();
+        this.context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        this.context.fill();
+    }
+}
+
+/*
+ HSL Explained:
+  Hue: degree on the colour wheel: 0-360 (0 = red, 120 = green, 240 = blue)
+  Saturation: percentage value: 0% = grey, 100% = full colour
+  Light: percentage value: 0% = white, 50% = full colour, 100% is black
+ */
